51aaf17e616721d49cf10d29ed4cb6a4b03d6664
Raghuraman Ramaswamy <raghuraman1@yahoo.com>
2024-01-24 03:01:14 +0530

    getting there

--- 6-first-fqn-example/src/main/java/com/example/FirstFqnExampleApplication.java
+++ 6-first-fqn-example/src/main/java/com/example/FirstFqnExampleApplication.java
@@ -6,12 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = { "io.github.xdamah", "com.example" })
-public class FirstExampleApplication {
-	private static final Logger logger = LoggerFactory.getLogger(FirstExampleApplication.class);
+public class FirstFqnExampleApplication {
+	private static final Logger logger = LoggerFactory.getLogger(FirstFqnExampleApplication.class);
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(FirstExampleApplication.class, args);
+		SpringApplication.run(FirstFqnExampleApplication.class, args);
 		logger.info("info message on startup");
 		logger.debug("debug message on startup");
 		logger.warn("warn message on startup");
