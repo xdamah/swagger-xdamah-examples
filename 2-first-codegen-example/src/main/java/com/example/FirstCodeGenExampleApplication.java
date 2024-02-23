0529e67ac1e8e09d3029547a5a154a8023254c1b
raghuraman1 <raghuraman1@yahoo.com>
2024-02-23 15:59:08 -0500

    Added a no codegen First example (#6)
    Added a no codegen First example

---
+++ 2-first-codegen-example/src/main/java/com/example/FirstCodeGenExampleApplication.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = { "io.github.xdamah", "com.example" })
+public class FirstCodeGenExampleApplication {
+	private static final Logger logger = LoggerFactory.getLogger(FirstCodeGenExampleApplication.class);
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FirstCodeGenExampleApplication.class, args);
+		logger.info("info message on startup");
+		logger.debug("debug message on startup");
+		logger.warn("warn message on startup");
+		logger.error("error message on startup");
+	}
+
+}
