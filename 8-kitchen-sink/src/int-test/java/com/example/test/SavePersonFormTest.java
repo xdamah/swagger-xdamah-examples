3792874b6f5ba829ca4dfd5891aeac5c08c3f28d
Raghuraman Ramaswamy <raghuraman1@yahoo.com>
2024-03-01 02:00:14 +0530

    will continute the test cases more later

--- 8-kitchen-sink/src/int-test/java/com/example/test/SavePersonFormTest.java
+++ 8-kitchen-sink/src/int-test/java/com/example/test/SavePersonFormTest.java
@@ -160,18 +160,18 @@ public class SavePersonFormTest {
 	}
 	
 	@Test
-	//TODO nested age does not validate
+	
 	void saveNestedPersonFormWithInvalidAgeAndOtherInvalidParamTest() throws Exception {
-		badRequest("person/i?def=17&defArr=1&defArr=2&defArr=3&x=2024-01-12", "examples/1.form.properties", this::invalidAgeInNested, "errors/invalidAgeNestedAndOtherParamsFrom.json");
+		badRequest("person/i?def=17&defArr=1&defArr=2&defArr=3&x=2024-01-12", "examples/2.form.properties", this::invalidAgeInNested, "errors/invalidAgeNestedAndOtherParamsFrom.json");
 	}
 	
 	
 	
 	
 	@Test
-	//custom validations like cc dont work till we provide them
+	
 	void savePersonFomWithInvalidCCAndOtherInvalidParamTest() throws Exception {
-		badRequest("person/i?def=17&defArr=1&defArr=2&defArr=3&x=2024-01-12", "examples/1.form.properties", this::invalidCard, "errors/invalidParamsWithNoSchemaForm.json");
+		badRequest("person/i?def=17&defArr=1&defArr=2&defArr=3&x=2024-01-12", "examples/1.form.properties", this::invalidCard, "errors/badccAndOtherParams.json");
 	}
 
 
