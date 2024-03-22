3e9bef4ce8e982eeb9cd8cd6018f26a8de687de7
Raghuraman Ramaswamy <raghuraman1@yahoo.com>
2024-03-22 23:59:55 +0530

    save

--- 08-polymorphism-alt-example/src/main/java/com/example/PolymosrphismAltExampleApplication.java
+++ 08-polymorphism-alt-example/src/main/java/com/example/PolymosrphismAltExampleApplication.java
@@ -1,13 +1,59 @@
 package com.example;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.github.xdamah.modelconverter.ByteArrayPropertyConverter;
+import io.github.xdamah.modelconverter.SubTypedPropertyConverter;
+import io.swagger.v3.core.converter.ModelConverter;
+import io.swagger.v3.core.converter.ModelConverters;
+import io.swagger.v3.core.jackson.ModelResolver;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication(scanBasePackages = { "io.github.xdamah", "com.example" })
 public class PolymosrphismAltExampleApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(PolymosrphismAltExampleApplication.class, args);
+	}
+	
+	@PostConstruct
+
+	  void adjustModelConverters() {
+	
+		List<ModelConverter> converters = ModelConverters.getInstance().getConverters();
+		//ModelConverters.getInstance().addConverter(new CustomOpenApiValidator(objectMapper));
+		ModelResolver modelResolver=null;
+		
+		converters = ModelConverters.getInstance().getConverters();
+		
+		
+		for (ModelConverter modelConverter : converters) {
+			
+			if(modelConverter instanceof ModelResolver)
+			{
+				modelResolver=(ModelResolver) modelConverter;
+			}
+			
+		}
+		ObjectMapper objectMapper=null;
+		if(modelResolver!=null)
+		{
+			objectMapper=modelResolver.objectMapper();
+		}
+		else
+		{
+			//
+			objectMapper= new ObjectMapper();
+		}
+		
+		
+		ModelConverters.getInstance().addConverter(new SubTypedPropertyConverter());
+		
 	}
 
 }
